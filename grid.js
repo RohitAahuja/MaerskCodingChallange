b698517fdb2dee18feb119934e2a6390ffe533e2
Rohit Aahuja <rohitaahuja@Rohits-MacBook-Air.local>
2022-06-25 01:15:43 +0530

    shuffle functionality done

---
+++ grid.js
@@ -0,0 +1,25 @@
+function buttonClickHandler(action) {
+  const container = document.getElementById("container");
+  const elementsArray = Array.prototype.slice.call(
+    container.getElementsByClassName("shuffle")
+  );
+  elementsArray.forEach((element) => container.removeChild(element));
+
+  action === 'shuffle' ? shuffleArray(elementsArray) : sortArray(elementsArray);
+  
+  elementsArray.forEach((element) => container.appendChild(element));
+}
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+function sortArray(array) {
+    
+}
